bdc4613821d32bf8723497dd5a72b257ab3b1085
pradeep-selva <pradeepselvaraju@gmail.com>
2022-11-28 18:33:01 +0530

    ft: safe deployment and module enabling

--- script/SafeDeploy.ts
+++ script/SafeDeploy.ts
@@ -1,15 +1,39 @@
 import { ethers } from "ethers";
 import dotenv from "dotenv";
+import EthersAdapter from "@gnosis.pm/safe-ethers-lib";
+import { SafeAccountConfig, SafeFactory } from "@gnosis.pm/safe-core-sdk";
 
 const main = async () => {
   dotenv.config();
+  /// replace with actual safe module address
+  const safeModule = "0x9633E0749faa6eC6d992265368B88698d6a93Ac0";
 
   const provider = new ethers.providers.JsonRpcProvider(process.env.MATIC_RPC);
-  const signer = await provider.getSigner(
-    "0x9633E0749faa6eC6d992265368B88698d6a93Ac0"
+  const safeOwner = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
+
+  const ethAdapter = new EthersAdapter({
+    ethers,
+    signerOrProvider: safeOwner
+  });
+
+  const safeFactory = await SafeFactory.create({
+    ethAdapter
+  });
+  const safeAccountConfig: SafeAccountConfig = {
+    owners: [safeOwner.address],
+    threshold: 1
+  };
+  const safeSdk = await safeFactory.deploySafe({ safeAccountConfig });
+  console.log("safe deployed to:", safeSdk.getAddress());
+
+  const safeEnableModuleTx = await safeSdk.createEnableModuleTx(safeModule);
+  const safeEnableModuleSignedTx = await safeSdk.signTransaction(
+    safeEnableModuleTx,
+    "eth_sign"
   );
 
-  console.log("address:", signer._address);
+  await safeSdk.executeTransaction(safeEnableModuleSignedTx);
+  console.log(`[Enabled Module] ${safeModule}`);
 };
 
 main()
