cf70111757111e3088fc892e5de4d7289bc1d0df
pradeep-selva <pradeepselvaraju@gmail.com>
2022-11-28 17:07:20 +0530

    ft: ethers setup

--- script/SafeDeploy.ts
+++ script/SafeDeploy.ts
@@ -1,1 +1,19 @@
-console.log("check");
+import { ethers } from "ethers";
+import dotenv from "dotenv";
+
+const main = async () => {
+  dotenv.config();
+
+  const provider = new ethers.providers.JsonRpcProvider(process.env.MATIC_RPC);
+  const signer = await provider.getSigner(
+    "0x9633E0749faa6eC6d992265368B88698d6a93Ac0"
+  );
+
+  console.log("address:", signer._address);
+};
+
+main()
+  .then(() => console.log("[Execution completed]"))
+  .catch((e) => {
+    throw e;
+  });
